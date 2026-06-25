e80cea84a6fb0689f97b04b825f4f352d2b38157
ayusshh66 <adonisjefrey66@gmail.com>
2026-06-25 15:17:52 +0530

    signup feature done

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import userRouter from './routes/user.routes.js'
 
 const app = express();
 const PORT = process.env.PORT ?? 8000;
 
+app.use(express.json())
 
 app.get('/', (req,res) => {
     return res.status(200).send(`server is readyyyy`)
 })
+
+app.use('/user', userRouter )
 
 app.listen(PORT, () => {
     return console.log(`the server is up and running at ${PORT}`)
