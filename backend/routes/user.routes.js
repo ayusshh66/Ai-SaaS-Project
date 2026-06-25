1f09b83aff7c01065a92195956e994f403d1fba4
ayusshh66 <adonisjefrey66@gmail.com>
2026-06-25 15:17:52 +0530

    signup feature done

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,11 +2,14 @@ import express from 'express';
 import { signUpValidation } from '../validators/signupValidation.js';
 import db from '../src/index.js';
 import { usersTable } from '../models/user.model.js';
-import { eq } from 'drizzle-orm';
+import { eq, or } from 'drizzle-orm';
+import { createHmac, randomBytes } from 'crypto';
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
 
-const route = express.Router();
+const router = express.Router();
 
-route.post('/signup', async(req,res) => {
+router.post('/signup', async(req,res) => {
     try {
 
     const request = await signUpValidation.safeParseAsync(req.body);
@@ -20,16 +23,33 @@ route.post('/signup', async(req,res) => {
     const [existingUser] = await db
                             .select()
                             .from(usersTable)
-                            .where(eq(usersTable.email === email) || eq(usersTable.userName === userName));
+                            .where(or(eq(usersTable.email,email), eq(usersTable.userName,userName)));
 
     if(existingUser){
-        return res.status(400).json({error : `the credentials ${existingUser} is already used, please try with different email or username`})
+        return res.status(400).json({error : `the credentials is already used, please try with different email or username`})
     } 
     
-    
+    const salt = randomBytes(256).toString('hex');
+    const hashedPassword = createHmac('sha256', salt).update(password).digest('hex')
 
+    const [user] = await db.insert(usersTable).values({
+        firstName,
+        lastName,
+        userName,
+        password : hashedPassword,
+        salt : salt,
+        email,
+    }).returning()
+
+    const token = jwt
+                    .sign({id:user.id, email : user.email, firstname : user.firstName }
+                    , process.env.JWT_SECRET);
+    
+    return res.status(201).json({status : `success`, data : {User_id : user.id, token : token, user : {name : firstName+" "+lastName, email : email}}})
 
     } catch (error) {
         console.error(error.message)
     }
 })
+
+export default router;
