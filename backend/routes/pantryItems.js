dd355d5cf58ac447507874754942a71e868affbb
ayusshh66 <adonisjefrey66@gmail.com>
2026-06-28 13:13:06 +0530

    fixed the bugs

--- backend/routes/pantryItems.js
+++ backend/routes/pantryItems.js
@@ -1,32 +1,39 @@
 import express from 'express'
 import { usersTable, pantryItemsTable } from '../models/user.model.js'
 import { pantryItemValidation } from '../validators/signupValidation.js';
+import db from '../src/index.js';
+import { authentication } from '../middleware/auth.js';
 
 
 const pantryRouter = express.Router();
 
-pantryRouter.post('/create', async(req,res) => {
+pantryRouter.post('/create', authentication,async(req,res) => {
     try {
         const request = await  pantryItemValidation.safeParseAsync(req.body);
 
+        if(request.error){
+            return res.status(400).json({error : request.error.format()})
+        }
 
         const {name, quantity, unit , category, expirey_date, is_running_low = false } = request.data;
         
-        const {relatedId} = req.user.id;
+        const relatedId = req.user.id;
+        const userName = req.user.name;
 
-        const [pantryItem] = await db.insert(pantryItemsTable).values({
+        const [pantryItems] = await db.insert(pantryItemsTable).values({
             userId :relatedId,
             name,
             quantity,
             unit,
             category,
-            expirey_date,
-            is_running_low,
+            expiryDate : expirey_date,
+            isRunningLow : is_running_low,
         }).returning();
 
-        return res.status(200).json({status : `new pantry items has been created`})
+        return res.status(200).json({status : `new pantry items has been created`, data : pantryItems, name : userName,})
     } catch (error) {
-        return console.error(error)
+        console.error(error)
+        return res.status(500).json({ error: "Internal server error" })
     }
 })
 
