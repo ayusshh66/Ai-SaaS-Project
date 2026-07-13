7405055ff205cafe7a1286a05788171cbf54acd2
ayusshh66 <adonisjefrey66@gmail.com>
2026-07-13 13:56:01 +0530

    made login and authprovider fucntion

--- frontend/context/AuthContext.jsx
+++ frontend/context/AuthContext.jsx
@@ -11,3 +11,42 @@ export const useAuth = () => {
     return context;
 };
 
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        // checks it user is logged in or nott
+        const token = localStorage.getItem('token');
+        const user = localStorage.getItem('user');
+
+        if(token && user){
+            setUser(JSON.parse(user))   
+        }
+
+        setLoading(false)
+
+    }, []);
+
+    const login = async (email, password) => {
+        try {
+
+            const response = await api.post('/api/users/login', {email, password});
+            const {user, token} = response.data.data;
+
+            localStorage.setItem('token', token);
+            localStorage.setItem('user', JSON.stringify(user));
+
+            setUser(user)
+
+            return { success : true}
+            
+        } catch (error) {
+            return {
+                success : false,
+                message : error.response?.data?.message || "login failed"
+            }
+        }
+    };
+
+}
