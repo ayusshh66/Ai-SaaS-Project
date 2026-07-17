f1455fb1a08cb10efafea4996ea6a8ce9fdacf5b
ayusshh66 <adonisjefrey66@gmail.com>
2026-07-17 15:36:52 +0530

    updated the user preferences table and model

--- backend/models/user.preferences.model.js
+++ backend/models/user.preferences.model.js
@@ -1,39 +1,30 @@
-import { pgTable,uuid, text, jsonb, timestamp, integer,boolean} from "drizzle-orm/pg-core";
-import { usersTable } from "./user.model.js"; 
+import { pgTable, uuid, text, jsonb, timestamp, integer } from "drizzle-orm/pg-core";
+import { usersTable } from "./user.model.js";
 import { relations } from "drizzle-orm";
 
 export const userPreferencesTable = pgTable("user_preferences", {
-  id: uuid("id").defaultRandom().primaryKey(),
-
-  userId: uuid("user_id")
-    .notNull()
-    .unique()
-    .references(() => usersTable.id, {
-      onDelete: "cascade",
-    }),
-
-  diet: text("diet"),
-
-  preferredCuisine: text("preferred_cuisine"),
-
-  spiceLevel: text("spice_level"),
-
-  allergies: jsonb("allergies"),
-
-  dailyCalories: integer("daily_calories"),
-
-  createdAt: timestamp("created_at").defaultNow(),
-
-  updatedAt: timestamp("updated_at").defaultNow(),
+    id: uuid("id").defaultRandom().primaryKey(),
+    userId: uuid("user_id")
+        .notNull()
+        .unique()
+        .references(() => usersTable.id, { onDelete: "cascade" }),
+    
+    dietaryRestrictions: jsonb("dietary_restrictions"), 
+    
+    preferredCuisines: jsonb("preferred_cuisines"),     
+    
+    defaultServings: integer("default_servings").default(2), 
+    
+    spiceLevel: text("spice_level"),
+    dailyCalories: integer("daily_calories"),
+    createdAt: timestamp("created_at").defaultNow(),
+    updatedAt: timestamp("updated_at").defaultNow(),
 });
 
-// making relation so that drizzle will know, and avoiding leftjoints
-export const userPreferencesRelations = relations(
-  userPreferencesTable,
-  ({ one }) => ({
+// Relational configuration for Drizzle
+export const userPreferencesRelations = relations(userPreferencesTable, ({ one }) => ({
     user: one(usersTable, {
-      fields: [userPreferencesTable.userId],
-      references: [usersTable.id],
+        fields: [userPreferencesTable.userId],
+        references: [usersTable.id],
     }),
-  })
-);
+}));
